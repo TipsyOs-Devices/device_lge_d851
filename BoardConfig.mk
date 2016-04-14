7163f1375a17bf486f78b70d06f5a6e54cba46df
Matthew Opperman <mattopp17@gmail.com>
2016-04-14 18:14:48 -0400

    update TC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,8 +28,8 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 TARGET_KERNEL_CONFIG := cyanogenmod_d851_defconfig
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9-uber/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
 # Optimizations
